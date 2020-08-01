4f667120b6b24e0c3ed32d9d4cf2aa798e5570c2
Mark Liu <ldtheended@163.com>
2020-08-01 18:35:19 +0800

    a1.71（example07——简单实现导航切换-同时路由组件中还可以嵌套标签）

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <h1>我的主页</h1>
     <div id="nav">
-      <router-link tag="button" to="/">Home</router-link>
+      <router-link exact tag="button" to="/">
+        <span> Home </span>
+      </router-link>
       <span> | </span>
-      <router-link tag="button" to="/about">About</router-link>
+      <router-link tag="button" to="/about">
+        <span> About </span>
+      </router-link>
     </div>
     <hr />
     <router-view></router-view>
@@ -19,7 +23,7 @@
 </script>
 
 <style>
-  .router-link-exact-active{
+  .router-link-active {
     color: red;
   }
 </style>
