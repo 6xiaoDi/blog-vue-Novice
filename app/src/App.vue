d4b847964b2130483181e08821b054b91574b15a
Mark Liu <ldtheended@163.com>
2020-08-01 18:15:44 +0800

    a1.68（example04——简单实现导航切换-换成button按钮）

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <h1>我的主页</h1>
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link tag="button" to="/">Home</router-link>
       <span> | </span>
-      <router-link to="/about">About</router-link>
+      <router-link tag="button" to="/about">About</router-link>
     </div>
     <hr />
     <router-view></router-view>
