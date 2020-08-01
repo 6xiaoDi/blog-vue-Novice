febe30dccc4926544e0af19be854bf2d5f8976e7
Mark Liu <ldtheended@163.com>
2020-08-01 12:32:20 +0800

    a1.57（example01-1——css无独立作用域的隐患）

--- vue脚手架demo/app/src/App.vue
+++ vue脚手架demo/app/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>软件开发</h1>
+    <p>前端Vue</p>
+    <hr>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
