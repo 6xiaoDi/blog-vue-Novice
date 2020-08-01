640ae74c1d0c3e65f06d3963ede53f7f1d342a23
Mark Liu <ldtheended@163.com>
2020-08-01 18:27:08 +0800

    a1.69（example05——简单实现导航切换-高亮切换）

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>我的主页</h1>
     <div id="nav">
-      <router-link tag="button" to="/">Home</router-link>
+      <router-link exact tag="button" to="/">Home</router-link>
       <span> | </span>
       <router-link tag="button" to="/about">About</router-link>
     </div>
@@ -17,3 +17,9 @@
     name: 'App'
   }
 </script>
+
+<style>
+  .router-link-active {
+    color: red;
+  }
+</style>
