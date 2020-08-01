e754c06e6707497c4f53068fa9a5f8523059ba64
Mark Liu <ldtheended@163.com>
2020-08-01 18:12:18 +0800

    a1.67（example03——简单实现导航切换）

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1>我的主页</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <span> | </span>
+      <router-link to="/about">About</router-link>
+    </div>
+    <hr />
     <router-view></router-view>
   </div>
 </template>
