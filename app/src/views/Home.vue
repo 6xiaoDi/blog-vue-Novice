220037f98ac5b8316a30574712df7a869e024b8b
Mark Liu <ldtheended@163.com>
2020-08-02 11:43:13 +0800

    a1.73（example01——axios接收数据出现跨域问题）

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -5,7 +5,25 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: "Home",
+
+        data() {
+            return {
+                items: []
+            }
+        },
+
+        async created() {
+            let rs= await axios({
+                url:'http://localhost:7777/items'
+            });
+
+            this.items = rs.data;
+
+            console.log(this.items);
+        }
     }
 </script>
