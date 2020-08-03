42aac6960696a014df1569b684425f1d7ba6c586
Mark Liu <ldtheended@163.com>
2020-08-03 22:52:02 +0800

    a2.09（example01-2——分页组件框子-优化样式）

--- app/src/components/Page.vue
+++ app/src/components/Page.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="pages">
-        <span>1</span>
+        <span class="current">1</span>
         <span>2</span>
         <span>3</span>
         <span>4</span>
@@ -19,5 +19,18 @@
 <style scoped>
     .pages {
         margin: 20px 0;
+    }
+
+    span {
+        text-align: center;
+        cursor: pointer;
+        padding: 10px 20px;
+        margin: 10px;
+        border: 1px solid #000000;
+    }
+    /*高亮当前页*/
+    span.current {
+        cursor: default;
+        background: rgba(0,255,255, .5);
     }
 </style>
